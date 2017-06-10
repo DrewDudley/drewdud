eae2b7287c1830a0275b345bd4f40f6f70a8bb90
Drew Dudley <drew.dudley@bullhorn.com>
2017-06-09 23:11:12 -0500

    Added font awesome and bootstrap, and progress on skill section

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -6,10 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./skills.component.scss']
 })
 export class SkillsComponent implements OnInit {
+  skills: Array<any>;
 
   constructor() { }
 
   ngOnInit() {
+    this.loadSkills();
   }
 
+  loadSkills() {
+    this.skills = [
+      {
+        name: "HTML",
+        description: "Description of HTML experience"
+      },
+      {
+        name: "CSS/SASS",
+        description: "Description of CSS experience"
+      },
+      {
+        name: "JaveScript/ TypeScript",
+        description: "Description of JS experience"
+      },
+      {
+        name: "Angular2",
+        description: "Description of ng2 experience"
+      }
+    ];
+  }
 }
