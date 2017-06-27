4aff3cc555e64dcdfc441fbed2add1ec7240e05e
Drew Dudley <drew.dudley@bullhorn.com>
2017-06-26 19:43:47 -0500

    Portfolio section

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -6,10 +6,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./portfolio.component.scss']
 })
 export class PortfolioComponent implements OnInit {
+  projects: Array<any>;
 
   constructor() { }
 
   ngOnInit() {
+    this.loadProjects();
+  }
+
+  loadProjects() {
+    this.projects = [
+      {
+        title: "Lighthouse Investments",
+        img: "../assets/img/portfolio/lhInvestments.png",
+        link: "#about"
+      },
+      {
+        title: "NightLight Branson",
+        img: "../assets/img/portfolio/office.jpg",
+        link: ""
+      },
+      {
+        title: "Bolivar Chamber of Commerce",
+        img: "../assets/img/portfolio/bcoc.png",
+        link: "#intro"
+      },
+      {
+        title: "Shuffle Ninja",
+        img: "../assets/img/portfolio/ninja.png",
+        link: ""
+      }
+    ]
   }
 
 }
